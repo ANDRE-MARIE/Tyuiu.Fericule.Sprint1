1e5f62e0e46c45d79df636801169c36129c1fd78
GNOLA25-1 <agdmdot@gmail.com>
2025-10-28 01:06:32 +0500

    Task5

---
+++ Tyuiu.Fericule.Sprint1.Task5.V5.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿
+using Tyuiu.Fericule.Sprint1.Task5.V5.Lib;
+
+namespace Tyuiu.Fericule.Sprint1.Task5.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            double x = 32.597;
+            DataService ds = new DataService();
+            double res = ds.Calculate(x);
+            _ = Convert.ToInt32(res);
+            int wait = 5;
+            Assert.AreEqual(wait, 0, 597);
+        }
+    }
+}
---
+++ Tyuiu.Fericule.Sprint1.Task5.V5/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using Tyuiu.Fericule.Sprint1.Task5.V5.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        {
+            ArgumentNullException.ThrowIfNull(args);
+            Console.WriteLine("*******************************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                                         *");
+            Console.WriteLine("*******************************************************************************************");
+
+            Console.WriteLine("Введите значение Х:");
+            double x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("*******************************************************************************************");
+
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                                               *");
+            Console.WriteLine("*******************************************************************************************");
+
+            Console.WriteLine("=== PROGRAMME EXTRACTION CHIFFRE FRACTIONNAIRE ===");
+
+            // Lecture du nombre réel positif
+            Console.Write("Entrez un nombre réel positif x : ");
+
+
+            // Vérification que le nombre est positif
+            if (x < 0)
+            {
+                Console.WriteLine("Erreur : Le nombre doit être positif !");
+                return;
+            }
+
+
+            // Utilisation de la bibliothèque pour obtenir le résultat
+            int premierChiffre = Convert.ToInt32(x);
+
+            // Affichage du résultat
+            Console.WriteLine($"Pour x = {x}, le premier chiffre de la partie fractionnaire est : {premierChiffre}");
+
+            Console.WriteLine("\nAppuyez sur une touche pour quitter...");
+            Console.ReadKey();
+        }
+    }
+}
