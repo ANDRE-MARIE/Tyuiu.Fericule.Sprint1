1e4281acdcbacc7369ba38a1515ed32cda06285d
GNOLA25-1 <agdmdot@gmail.com>
2025-10-27 21:15:30 +0500

    Ajoutez des fichiers projet.

---
+++ Tyuiu.Fericule.Sprint1.Task0.V15.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿
+using Tyuiu.Fericule.Sprint1.Task0.V15.Lib;
+
+namespace Tyuiu.Fericule.Sprint1.Task0.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(3.0, res); // ou bien Assert.AreEqual(3, res) fonctionnera car 3.0 == 3
+        }
+    }
+}
---
+++ Tyuiu.Fericule.Sprint1.Task0.V15/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using Tyuiu.Fericule.Sprint1.Task0.V15.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        double result = ds.Calculate();
+        Console.WriteLine("Le résultat de l'expression (30/6-4)*3 est : " + result); ;
+    }
+}
