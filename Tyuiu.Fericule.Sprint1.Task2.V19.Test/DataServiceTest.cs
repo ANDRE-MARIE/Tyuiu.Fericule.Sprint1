bc7415c6de3bd292dab36179fd0d1788f4cce04a
GNOLA25-1 <agdmdot@gmail.com>
2025-10-27 22:36:02 +0500

    Task2

---
+++ Tyuiu.Fericule.Sprint1.Task2.V19.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Tyuiu.Fericule.Sprint1.Task2.V19.Lib;
+
+namespace Tyuiu.Fericule.Sprint1.Task2.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void InchesToMeters_ShouldConvertCorrectly(int input, double expected)
+        {
+            var converter = new DataService();
+            double result = converter.ConvertInchToKm(input);
+            Assert.AreEqual(expected, result, 3); // précision à 3 décimales
+        }
+    }
+    }
+
---
+++ Tyuiu.Fericule.Sprint1.Task2.V19/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using Tyuiu.Fericule.Sprint1.Task2.V19.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var converter = new DataService();
+
+        Console.WriteLine("Entrez la distance en pouces :");
+        int inches = Convert.ToInt32(Console.ReadLine());
+
+        double km = converter.ConvertInchToKm(inches);
+        Console.WriteLine($"Distance en kilomètres : {km}");
+    }
+}
